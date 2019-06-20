9721d7886ecdae1f2d0ce14012ff5fcbb10fe7ba
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2019-06-20 22:23:32 +0700

    update UI

---
+++ public/precache-manifest.427422d6b8f26ef591302aad549c7303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e015c6707ef6b7ccb7bde6d21eab7eec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa2c88492b637db68dfc",
+    "url": "/static/css/main.1e7a38af.chunk.css"
+  },
+  {
+    "revision": "1669d318b7b83fd21f8d",
+    "url": "/static/js/2.d8662bd8.chunk.js"
+  },
+  {
+    "revision": "aa2c88492b637db68dfc",
+    "url": "/static/js/main.edfc8bd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58b9b2ebadb987820dfeab6bbbbb9ce3.js"
+  "/precache-manifest.427422d6b8f26ef591302aad549c7303.js"
 );
 
 self.addEventListener('message', (event) => {
